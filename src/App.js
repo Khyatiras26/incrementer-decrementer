a6a62f2e8ac76d7bb07cc10900098c70749b4e60
Khyati <khyati2609@gmail.com>
2023-05-20 12:49:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import UseReducer from './hooks/useReducer';
+import './App.css';
+// import UseState from './component/Basics/hooks/useState'
+// import UseEffect from './component/Basics/hooks/useEffect'
+
+function App() {
+  return (
+    <div className="App">
+      <UseReducer/>
+    </div>
+  );
+}
+
+export default App;
